db5580937208b67550427a78a7d38cef728d4e97
takayasumorishita <c011729632@edu.teu.ac.jp>
2022-02-03 23:59:38 +0900

    Add flags

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include<unistd.h>
+#include<sched.h>
 
 int main(int argc, char const **argv)
 {
+	// exec
 	const char *path = "/bin/sh";
 	char *const *argv2;
 	execv(path, argv2);
+
+	// unshare
+	int flags = CLONE_NEWIPC | CLONE_NEWNET | CLONE_NEWUSER;
+	unshare(flags);
 	return 0;
 }
